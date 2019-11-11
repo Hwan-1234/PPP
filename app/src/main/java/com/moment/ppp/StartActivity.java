7644c57a4d5e514f27a04b38e52deb030bd81844
Hwan-1234 <mwwo88@gmail.com>
2019-11-11 11:02:16 +0900

    FFF

--- app/src/main/java/com/moment/ppp/StartActivity.java
+++ app/src/main/java/com/moment/ppp/StartActivity.java
@@ -3,6 +3,7 @@ package com.moment.ppp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -12,4 +13,18 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
     }
 
+    public void clickMain(View view) {
+    }
+
+    public void clickChat(View view) {
+    }
+
+    public void clickGallery(View view) {
+    }
+
+    public void clickAPI(View view) {
+    }
+
+    public void clickImg(View view) {
+    }
 }
