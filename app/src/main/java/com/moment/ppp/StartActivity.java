5128d4ce83489a19c2d7385e00c2a6b3ca27c36f
Hwan-1234 <mwwo88@gmail.com>
2019-11-11 09:13:39 +0900

    FFF

---
+++ app/src/main/java/com/moment/ppp/StartActivity.java
@@ -0,0 +1,15 @@
+package com.moment.ppp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+}
