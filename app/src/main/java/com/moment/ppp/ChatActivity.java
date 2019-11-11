572ca9fad8f7db8d181e82a0e1cce7a2fcbb8c4d
Hwan-1234 <mwwo88@gmail.com>
2019-11-11 13:05:40 +0900

    FFF

---
+++ app/src/main/java/com/moment/ppp/ChatActivity.java
@@ -0,0 +1,14 @@
+package com.moment.ppp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
--- app/src/main/java/com/moment/ppp/StartActivity.java
+++ app/src/main/java/com/moment/ppp/StartActivity.java
@@ -2,29 +2,51 @@ package com.moment.ppp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
-
+    Button chat,gallery,api,user,a;
+    ImageButton id,myid,aid;
+    TextView tvmy,tva,tvday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
 
-    public void clickMain(View view) {
+
+    public void Chat(View view) {
+        Intent intent=new Intent(this,ChatActivity.class);
+        startActivity(intent);
     }
 
-    public void clickChat(View view) {
+    public void Gallery(View view) {
+        Intent intent=new Intent(this,GalleryActivity.class);
+        startActivity(intent);
     }
 
-    public void clickGallery(View view) {
+    public void API(View view) {
+        Intent intent=new Intent(this,ApiActivity.class);
+        startActivity(intent);
     }
 
-    public void clickAPI(View view) {
+    public void Img(View view) {
     }
 
-    public void clickImg(View view) {
+    public void User(View view) {
+    }
+
+    public void A(View view) {
+    }
+
+    public void aProfile(View view) {
+    }
+
+    public void myProfile(View view) {
     }
 }
