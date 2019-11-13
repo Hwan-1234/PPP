69a5e028af1322df50869fd5dbb472cd76875387
Hwan-1234 <mwwo88@gmail.com>
2019-11-13 11:00:51 +0900

    FFF

---
+++ app/src/main/java/com/moment/ppp/H.java
@@ -0,0 +1,9 @@
+package com.moment.ppp;
+
+public class H {
+    public static final int REQUEST_CODE = 0;
+    public static String name;
+    public static String frofileImg;
+
+
+}
--- app/src/main/java/com/moment/ppp/StartActivity.java
+++ app/src/main/java/com/moment/ppp/StartActivity.java
@@ -1,15 +1,26 @@
 package com.moment.ppp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+
 
 public class StartActivity extends AppCompatActivity {
+
     Button chat,gallery,api,user,a;
     ImageButton id,myid,aid;
     TextView tvmy,tva,tvday;
@@ -17,6 +28,45 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        id=findViewById(R.id.id);
+
+        id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, H.REQUEST_CODE);
+                int permissionCheck = ContextCompat.checkSelfPermission(StartActivity.this, Manifest.permission.WRITE_CALENDAR);
+
+                if(permissionCheck== PackageManager.PERMISSION_DENIED){
+
+                    // 권한 없음
+                }else{
+                    // 권한 있음
+                }
+            }
+        });
+
+    }    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == H.REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    id.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 
@@ -26,9 +76,7 @@ public class StartActivity extends AppCompatActivity {
     }
 
     public void Gallery(View view) {
-        Intent intent=new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");// '*' 모든 확장자
-        startActivityForResult(intent,10);
+
     }
 
     public void API(View view) {
@@ -38,7 +86,13 @@ public class StartActivity extends AppCompatActivity {
 
     public void Img(View view) {
 
+
+
+
+
     }
+
+
 
     public void User(View view) {
 
@@ -53,5 +107,8 @@ public class StartActivity extends AppCompatActivity {
     }
 
     public void myProfile(View view) {
+
     }
 }
+
+
